73306dba3dd4557696ef895308e518955bba1098
derry derajat <derryderajat@mail.com>
2022-10-05 12:00:58 +0700

    add heder

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,12 @@
+import Head from 'next/head';
+import Header from '../components/Header';
 export default function Home() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <div className='bg-[#232323] h-screen text-[#d4d4d4] snap-y snap-mandatory w-full overflow-scroll z-0'>
+      <Head>
+        <title>Derry&#39;s Portofolio</title>
+      </Head>
+      <Header />
+    </div>
+  );
 }
