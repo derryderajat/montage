f54676e23b23aec89530769e4d1fc6ab198018cb
derry derajat <derryderajat@mail.com>
2022-10-05 12:47:38 +0700

    add hover coolor icon header

--- components/Header.js
+++ components/Header.js
@@ -20,26 +20,30 @@ function Header() {
       >
         {/* Social Icons */}
         <SocialIcon
+          className='cursor-pointer hover:text-[#00DEE6] text-[#d4d4d4]'
           url='https://www.instagram.com/__derryd__/'
-          fgColor='gray'
+          fgColor='currentColor'
           bgColor='transparent'
         />
         {/* Social Icons */}
         <SocialIcon
+          className='cursor-pointer hover:text-[#00DEE6] text-[#d4d4d4]'
           url='https://github.com/derryderajat/'
-          fgColor='gray'
+          fgColor='currentColor'
           bgColor='transparent'
         />
         {/* Social Icons */}
         <SocialIcon
+          className='cursor-pointer hover:text-[#00DEE6] text-[#d4d4d4]'
           url='https://www.linkedin.com/in/derryderajat/'
-          fgColor='gray'
+          fgColor='currentColor'
           bgColor='transparent'
         />
         {/* Social Icons */}
         <SocialIcon
+          className='cursor-pointer hover:text-[#00DEE6] text-[#d4d4d4]'
           url='https://www.instagram.com/__derryd__/'
-          fgColor='gray'
+          fgColor='currentColor'
           bgColor='transparent'
         />
       </motion.div>
@@ -55,15 +59,15 @@ function Header() {
           scale: 1,
         }}
         transition={{ duration: 1 }}
-        className='flex flex-row items-center text-lightGray'
+        className='flex flex-row items-center '
       >
         <SocialIcon
-          className='cursor-pointer'
+          className='cursor-pointer hover:text-[#00DEE6] text-[#d4d4d4]'
           network='email'
-          fgColor='gray'
+          fgColor='currentColor'
           bgColor='transparent'
         />
-        <p className='uppercase hidden md:block text-sm text-lightGray'>
+        <p className='uppercase hidden md:block text-sm text-[#d4d4d4]  hover:text-[#00DEE6] hover:cursor-pointer'>
           get in touch
         </p>
       </motion.div>
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Header from '../components/Header';
 export default function Home() {
   return (
-    <div className='bg-[#232323] h-screen text-[#d4d4d4] snap-y snap-mandatory w-full overflow-scroll z-0'>
+    <div className='bg-[#151516] h-screen text-[#d4d4d4] snap-y snap-mandatory w-full overflow-scroll z-0'>
       <Head>
         <title>Derry&#39;s Portofolio</title>
       </Head>
