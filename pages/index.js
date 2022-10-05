8c4c1843d303ac741009699a3bc62e4bde83fbcb
derry derajat <derryderajat@mail.com>
2022-10-05 17:02:50 +0700

    add hero section

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Header from '../components/Header';
+import Hero from '../components/Hero';
 export default function Home() {
   return (
     <div className='bg-[#151516] h-screen text-[#d4d4d4] snap-y snap-mandatory w-full overflow-scroll z-0'>
@@ -7,6 +8,9 @@ export default function Home() {
         <title>Derry&#39;s Portofolio</title>
       </Head>
       <Header />
+      <section id='hero' className='snap-center'>
+        <Hero />
+      </section>
     </div>
   );
 }
